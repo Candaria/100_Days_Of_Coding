b0055ad4b5bf3d1fa15c50ce1683ccf099b5d7fc
Candaria Candaria <candariamajid@gmail.com>
2023-10-12 16:17:27 +0800

    Day4

---
+++ Day4.java
@@ -0,0 +1,12 @@
+
+public class Day4 {
+
+    public static void main(String[] args) {
+        int angka1 = 2;
+        int angka2 = 5;
+
+        int angka3 = angka1 + angka2;
+
+        System.out.println("Hasil penjumlahan adalah: " + angka3);
+    }
+}
