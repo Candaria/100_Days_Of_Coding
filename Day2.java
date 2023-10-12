e5aa41cd0681a3f1e3cb06077fe18d1bdc8a9c02
Candaria Candaria <candariamajid@gmail.com>
2023-10-12 16:08:31 +0800

    first commit

---
+++ Day2.java
@@ -0,0 +1,8 @@
+public class Day2 {
+    public static void main(String[] args) {
+        String nama = "Candaria";
+        int umur = 20;
+        System.out.println("nama saya " + nama );
+        System.out.println("umur saya " + umur + " tahun ");
+    }
+}
---
+++ Day3.java
@@ -0,0 +1,10 @@
+public class Day3 {
+    public static void main(String[] args) {
+        int panjang = 10;
+        int lebar = 5;
+
+        int luas = panjang * lebar;
+
+        System.out.println("Luas persegi panjang: " + luas);
+    }
+}
