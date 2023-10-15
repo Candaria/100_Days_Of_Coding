37493a2b119a5a0c154b3d53a97cf0eadfc3ed14
Candaria Candaria <candariamajid@gmail.com>
2023-10-15 11:08:52 +0800

    Day6

---
+++ Day6.java
@@ -0,0 +1,27 @@
+
+public class Day6 {
+
+    public static void main(String[] args) {
+
+        String namaBapak = "Abdul Majid";
+        int umurBapak = 47;
+
+        String namaIbu = "Sati";
+        int umurIbu = 42;
+
+        String namaKakak = "Muh.Asnur";
+        int umurKakak = 21;
+
+        System.out.println("Biodata Keluarga:");
+        System.out.println("-----------------");
+        System.out.println("Nama Bapak : " + namaBapak);
+        System.out.println("Umur: " + umurBapak);
+        System.out.println();
+        System.out.println("Nama Ibu : " + namaIbu);
+        System.out.println("Umur: " + umurIbu);
+        System.out.println();
+        System.out.println("Nama Kakak : " + namaKakak);
+        System.out.println("Umur: " + umurKakak);
+
+    }
+}
