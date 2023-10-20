f557e073cfc7fe5f42ad04c20fe73112fa623f6f
Candaria Candaria <candariamajid@gmail.com>
2023-10-20 16:34:33 +0800

    Percabangan if else

---
+++ Day12.java
@@ -0,0 +1,12 @@
+public class Day12 {
+
+    public static void main(String[] args) {
+        int nilai = 7;
+
+        if (nilai > 5) {
+            System.out.println("Nilai lebih besar dari 5");
+        } else {
+            System.out.println("Nilai kurang dari atau sama dengan 5");
+        }
+    }
+}
