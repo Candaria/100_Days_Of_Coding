a9d82208280d07e8c63c231608c0ca59865cdf04
Candaria Candaria <candariamajid@gmail.com>
2023-10-22 07:16:58 +0800

    Membandingkan angka

---
+++ Day13.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Day13 {
+
+    public static void main(String[] args) {
+        Scanner nilai = new Scanner(System.in);
+
+        System.out.println("Masukkan angka : ");
+        int nilai1 = nilai.nextInt();
+
+        if (nilai1 > 5) {
+            System.out.println("Nilai lebih besar dari 5");
+
+        } else {
+            System.out.println("Nilai kurang dari atau sama dengan 5");
+        }
+    }
+}
