e4e1fbc8e6c41cb7f5fa4451945504f2bc3cc3a4
Candaria Candaria <candariamajid@gmail.com>
2023-10-24 13:33:53 +0800

    perulangan

---
+++ Day15.java
@@ -0,0 +1,10 @@
+
+public class Day15 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Angka: " + i);
+        }
+    }
+
+}
