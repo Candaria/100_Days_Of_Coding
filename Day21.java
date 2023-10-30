2a13a3948813e80ef99fe5d9caf2a1d9375a211c
Candaria Candaria <candariamajid@gmail.com>
2023-10-30 19:59:15 +0800

    if else inputan dengan menambahkan perintah while

---
+++ Day21.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Day21 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Masukkan bilangan :");
+            int bilangan = input.nextInt();
+
+            if (bilangan % 2 == 0) {
+                System.out.println("Bilangan genap. Silakan masukkan kembali");
+            } else {
+                System.out.println("You and I, end.");
+                break;
+            }
+        }
+    }
+}
