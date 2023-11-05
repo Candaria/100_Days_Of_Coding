8520656b6cf99f9f826b9dc7cfdaa51bfdecab62
Candaria Candaria <candariamajid@gmail.com>
2023-11-05 08:35:23 +0800

    Menentukan bilangan ganjil dan genap

---
+++ Day26.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Day26 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = scanner.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println(angka + " adalah bilangan genap.");
+        } else {
+            System.out.println(angka + " adalah bilangan ganjil.");
+        }
+
+        scanner.close();
+    }
+}
