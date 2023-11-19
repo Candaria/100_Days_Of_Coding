fac7c02dc43e976a57472669f3806429d74bc00e
Candaria Candaria <candariamajid@gmail.com>
2023-11-19 17:54:58 +0800

    Membuatn dua inputan yaitu nilai awal dan nilai akhir lalu akan mencetak keterangan angka 10 adalah bilangan genap dan angka 9 adalah bilangan ganjil dan seterusnya

---
+++ Day42.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Day42 {
+
+    public static void main(String[] args) {
+
+        Scanner an = new Scanner(System.in);
+        System.out.println("Masukkan nilai awal : ");
+        int angkaawal = an.nextInt();
+        System.out.println("Masukkan nilai akhir : ");
+        int angkaakhir = an.nextInt();
+
+        for (int i = angkaawal; i <= angkaakhir; i++) {
+            if (i % 2 == 0) {
+                System.out.println("nilai ini adalah bilangan genap : " + i);
+            }
+
+        }
+        for (int i = angkaawal; i <= angkaakhir; i++) {
+            if (i % 2 != 0) {
+                System.out.println("nilai ini adalah bilangan ganjil : " + i);
+            }
+
+        }
+    }
+}
