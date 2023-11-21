1519bd1ed7ada0a57db027ec19205d3ec845ef4c
Candaria Candaria <candariamajid@gmail.com>
2023-11-22 07:22:05 +0800

    Bilangan yang tidak habis dibagi 5

---
+++ Day43.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Day43 {
+
+    public static void main(String[] args) {
+        Scanner angka = new Scanner(System.in);
+
+        System.out.println("Masukkan nilai awal : ");
+        int awal = angka.nextInt();
+        System.out.println("Masukkan nilai akhir : ");
+        int akhir = angka.nextInt();
+
+        for (int i = awal; i <= akhir; i++) {
+            if (i % 5 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
