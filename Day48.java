9171cf4a5352681dd2d9537de9603950fbe77920
Candaria Candaria <candariamajid@gmail.com>
2023-11-26 14:08:21 +0800

    Menampilkan nama benda dengan array

--- Day48.java
+++ Day48.java
@@ -3,12 +3,12 @@ public class Day48 {
 
     public static void main(String[] args) {
 
-        int[] arrayAngka = {1, 2, 3, 4, 5};
+        int[] arrayAng = {1, 2, 3, 4, 5};
 
         System.out.println("Elemen-elemen array:");
 
-        for (int i = 0; i < arrayAngka.length; i++) {
-            System.out.println("Elemen ke-" + i + ": " + arrayAngka[i]);
+        for (int i = 0; i < arrayAng.length; i++) {
+            System.out.println("Elemen ke-" + i + ": " + arrayAng[i]);
         }
     }
 }
---
+++ Day49.java
@@ -0,0 +1,12 @@
+
+public class Day49 {
+
+    public static void main(String[] args) {
+
+        String[] namaBenda = {"Jam", "Kursi", "Sepatu", "Gelas", "Sendok"};
+
+        for (String nama : namaBenda) {
+            System.out.println(nama);
+        }
+    }
+}
