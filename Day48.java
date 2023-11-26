785cbe506c6dcca2cd86938951f1b17127d0b461
Candaria Candaria <candariamajid@gmail.com>
2023-11-26 14:05:21 +0800

    Menampilkan elemen array

---
+++ Day48.java
@@ -0,0 +1,14 @@
+
+public class Day48 {
+
+    public static void main(String[] args) {
+
+        int[] arrayAngka = {1, 2, 3, 4, 5};
+
+        System.out.println("Elemen-elemen array:");
+
+        for (int i = 0; i < arrayAngka.length; i++) {
+            System.out.println("Elemen ke-" + i + ": " + arrayAngka[i]);
+        }
+    }
+}
